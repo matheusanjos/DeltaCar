d2c6e545fb14a73856c246014d3103d0a8afc891
Matheus Rodrigues <matheushranjos@gmail.com>
2018-12-11 20:01:09 -0200

    Correção

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -20,7 +20,7 @@
           <div class="card mb-3">
             <div class="card-header">
               <i class="fas fa-chart-area"></i>
-              Area Chart Example</div>
+              Últimos carros alugados</div>
             <div class="card-body">
               <canvas id="myAreaChart" width="100%" height="30"></canvas>
             </div>
